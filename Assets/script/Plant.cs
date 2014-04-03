13b2028724e5c44be3311b6324e34ba5e20cc795
Manuel Finessi <fino.manu@gmail.com>
2014-04-03 23:43:54 +0200

    script water, plant, tempControl - INIT

---
+++ Assets/script/Plant.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plant : MonoBehaviour {
+
+	Water water;
+	TempControl tempControl;
+
+	void Awake()
+	{
+		water = (Water)GetComponent<Water>();
+		tempControl = (TempControl)GetComponent<TempControl>();
+	}
+
+	void Start () 
+	{
+		Debug.Log("NPK: " + water.N + "-" + water.P + "-" + water.K);
+		Debug.Log("PH :" + water.PH_Value);
+		Debug.Log("Temp :" + tempControl.temperature);
+	}
+	
+
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/script/Water.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water : MonoBehaviour {
+
+	public float N,P,K;
+	public float PH_Value;
+	public float EC_Value;
+
+	void Start () 
+	{
+
+	}
+	
+
+	void Update () 
+	{
+	
+	}
+}
