2a8072c003e6c8a7c65ebcee41be45860670c7f3
Manuel Finessi <fino.manu@gmail.com>
2014-04-04 13:19:22 +0200

    HUD GUI, basic test

---
+++ Assets/script/hudPlace.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hudPlace : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<GUITexture>().enabled = true;
+		transform.position = new Vector3(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
