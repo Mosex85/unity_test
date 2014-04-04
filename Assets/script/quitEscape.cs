62f2b1a8adb2187dcd990811a75e3a4939d8993f
Manuel Finessi <fino.manu@gmail.com>
2014-04-04 22:23:43 +0200

    Pause Menu, INIT

--- Assets/script/quitEscape.cs
+++ Assets/script/quitEscape.cs
@@ -2,8 +2,49 @@
 using System.Collections;
 
 public class quitEscape : MonoBehaviour {
+
+	private MouseLook ms1, ms2;
+	private mirino mir;
+
+	private GameObject gobj, escText;
+	private bool isPaused = false;
+
+	private GUITexture tex;
+
+	void Start()
+	{
+		ms1 = (MouseLook)GetComponent<MouseLook>();
+		mir = (mirino)GetComponent<mirino>();
+
+		gobj = GameObject.FindGameObjectWithTag("Player");
+		ms2 = (MouseLook)gobj.GetComponent<MouseLook>();
+
+		escText = GameObject.Find("escTexture");
+
+		tex = (GUITexture)escText.GetComponent<GUITexture>();
+	}
+
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape))
-			Application.Quit();
+		if (Input.GetKeyDown(KeyCode.Escape)){
+
+			if(isPaused == false) {
+				ChangeState(false);
+
+				Application.Quit(); // FIX
+			} else {
+				ChangeState(true);
+			}
+		}
+	}
+
+	void ChangeState(bool b)
+	{
+		Screen.showCursor = !b;
+		ms1.enabled = b;
+		mir.enabled = b;
+		ms2.enabled = b;
+		tex.enabled = !b;
+
+		isPaused = !isPaused;
 	}
 }
